02e424be89e48a52ac0c4d573f1fa1810896b6fb
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-07-25 06:38:13 +0100

    Convenient utilities for performing reflection

---
+++ src/main/java/com/lauriethefish/betterportals/ReflectUtils.java
@@ -0,0 +1,30 @@
+package com.lauriethefish.betterportals;
+
+import java.lang.reflect.Field;
+
+public class ReflectUtils {
+    
+    // Sets a field in the given object, even if it is private
+    public static void setField(Object obj, String name, Object value)    {
+        try {
+            Field field = obj.getClass().getField(name);
+            field.setAccessible(true);
+            field.set(obj, value);
+        }   catch(NoSuchFieldException | IllegalAccessException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Gets the value of the named field. Will return null if the field doesn't exist, or is of the wrong type
+    @SuppressWarnings("unchecked")
+    public static <T> T getField(Object obj, String name)   {
+        try {
+            Field field = obj.getClass().getField(name);
+            field.setAccessible(true);
+            return (T) field.get(obj);
+        }   catch(NoSuchFieldException | IllegalAccessException | ClassCastException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
