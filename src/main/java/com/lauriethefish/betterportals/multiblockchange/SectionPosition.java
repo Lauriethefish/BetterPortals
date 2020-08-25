82bd03fc1986cd18f3261182ab7fbfed4cd8099e
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-08-25 15:23:12 +0100

    New class for representing NMS ChunkSections.

---
+++ src/main/java/com/lauriethefish/betterportals/multiblockchange/SectionPosition.java
@@ -0,0 +1,40 @@
+package com.lauriethefish.betterportals.multiblockchange;
+
+import java.util.Objects;
+
+import com.lauriethefish.betterportals.ReflectUtils;
+
+import org.bukkit.util.Vector;
+
+public class SectionPosition {
+    public int x;
+    public int y;
+    public int z;
+    public SectionPosition(Vector location)    {
+        x = location.getBlockX() >> 4;
+        x = location.getBlockY() >> 4;
+        z = location.getBlockZ() >> 4;
+    }
+
+    // Make an NMS SectionPosition from this object
+    public Object toNMS()   {
+        return ReflectUtils.newInstance("SectionPosition", new Class[]{int.class, int.class, int.class}, new Object[]{x, y, z});
+    }
+
+    // Automatically generated
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof SectionPosition)) {
+            return false;
+        }
+        SectionPosition sectionPosition = (SectionPosition) o;
+        return x == sectionPosition.x && y == sectionPosition.y && z == sectionPosition.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+}
