51c8b16efaa1ef7e56fcea11ddbe9595a57c0b22
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-09-08 06:29:10 +0100

    Changed around some methods for getting IBlockData

--- src/main/java/com/lauriethefish/betterportals/BlockRaycastData.java
+++ src/main/java/com/lauriethefish/betterportals/BlockRaycastData.java
@@ -9,40 +9,38 @@ import org.bukkit.util.Vector;
 
 // Stores data about a block surrounding the portal that is in the HashMap of viewable blocks
 public class BlockRaycastData {
-    public static Object edgeData = null;
+    public static Object edgeData = makeEdgeData();
+    private static Object makeEdgeData()    {
+        // Different colours of concrete depend on version
+        if(ReflectUtils.isLegacy)  {
+            return getNMSData(Material.valueOf("CONCRETE"), (byte) 15);
+        }   else    {
+            return getNMSData(Material.BLACK_CONCRETE);
+        }
+    }
 
     public Vector originVec;
     public Object originData;
     public Object destData;
     public BlockRaycastData(Location originLoc, Location destLoc, boolean edge)   {
-        if(edgeData == null)    {
-            // Different colours of concrete depend on version
-            if(ReflectUtils.isLegacy)  {
-                edgeData = getNMSData(251, (byte) 15);
-            }   else    {
-                edgeData = getNMSData(Material.BLACK_CONCRETE);
-            }
-        }
-
         this.originVec = PlayerRayCast.moveVectorToCenterOfBlock(originLoc.toVector());
         this.originData = getNMSData(originLoc.getBlock());
         this.destData = edge ? edgeData : getNMSData(destLoc.getBlock());
     }
 
-    // Used if in a legacy version
-    public Object getNMSData(int id, byte data)    {
-        int combined = id + (data << 12);
-        return ReflectUtils.runMethod(null, ReflectUtils.getMcClass("Block"), "getByCombinedId", new Class[]{int.class}, new Object[]{combined});
-    }
-
-    // Used if in a modern version
-    public Object getNMSData(Material mat)  {
+    public static Object getNMSData(Material mat)  {
         Object block = ReflectUtils.runMethod(null, ReflectUtils.getBukkitClass("util.CraftMagicNumbers"), "getBlock", new Class[]{Material.class}, new Object[]{mat});
         return ReflectUtils.getField(block, "blockData");
     }
 
+    @SuppressWarnings("deprecation")
+    public static Object getNMSData(Material mat, byte data)    {
+        int combinedId = mat.getId() + (data << 12);
+        return ReflectUtils.runMethod(null, ReflectUtils.getMcClass("Block"), "getByCombinedId", new Class[]{int.class}, new Object[]{combinedId});
+    }
+
     // Finds the NMS IBlockData from a bukkit block, this differs depending on if using a legacy version or modern version
-    public Object getNMSData(Block block)   {
+    public static Object getNMSData(Block block)   {
         if(ReflectUtils.isLegacy)  {
             return ReflectUtils.runMethod(block, "getData0");
         }   else    {
