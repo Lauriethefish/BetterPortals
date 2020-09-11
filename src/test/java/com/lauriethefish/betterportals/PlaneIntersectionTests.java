fd420bcaefa7144b638c0db4db84cdc1455f9981
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-09-11 07:59:48 +0100

    Unit tests for new math classes

---
+++ src/test/java/com/lauriethefish/betterportals/PlaneIntersectionTests.java
@@ -0,0 +1,30 @@
+package com.lauriethefish.betterportals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.lauriethefish.betterportals.math.PlaneIntersectionChecker;
+
+import org.bukkit.util.Vector;
+import org.junit.jupiter.api.Test;
+
+public class PlaneIntersectionTests {
+    @Test
+    void simpleTestVisible()    {
+        // This plane will point directly upwards
+        PlaneIntersectionChecker checker = new PlaneIntersectionChecker(
+                        new Vector(0.0, 0.0, 0.0), new Vector(1.0, 0.0, 0.0), new Vector(10.0, 0.0, 0.0));
+
+        Vector blockPos = new Vector(-5.0, 0.0, 0.0);
+        assertTrue(checker.checkIfVisibleThroughPortal(blockPos), "Block returned as not visible when it was");
+    }
+
+    @Test
+    void simpleTestNotVisible()    {
+        // This plane will point directly upwards
+        PlaneIntersectionChecker checker = new PlaneIntersectionChecker(
+                        new Vector(0.0, 0.0, 0.0), new Vector(1.0, 0.0, 0.0), new Vector(10.0, 0.0, -10.0));
+
+        Vector blockPos = new Vector(-5.0, 0.0, 20.0);
+        assertFalse(checker.checkIfVisibleThroughPortal(blockPos), "Block returned as visible when it wasn't");
+    }
+}
