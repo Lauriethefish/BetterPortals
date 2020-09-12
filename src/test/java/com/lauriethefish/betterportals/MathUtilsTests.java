5a71759a3d3148bf8587a913f8384da8f065488a
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-09-12 08:35:51 +0100

    Tests for MathUtils

---
+++ src/test/java/com/lauriethefish/betterportals/MathUtilsTests.java
@@ -0,0 +1,39 @@
+package com.lauriethefish.betterportals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.lauriethefish.betterportals.math.MathUtils;
+
+import org.bukkit.util.Vector;
+import org.junit.jupiter.api.Test;
+
+// Tests for the MathUtils class
+public class MathUtilsTests {
+    @Test
+    void roundingTest() {
+        Vector input = new Vector(1.5, 2.6, 1.1);
+        Vector rounded = MathUtils.round(input);
+
+        assertEquals(rounded, new Vector(2.0, 3.0, 1.0));
+    }
+
+    @Test
+    void absoluteTest() {
+        Vector input = new Vector(-1.5, 2.6, -1.1);
+        Vector absolute = MathUtils.abs(input);
+
+        assertEquals(absolute, new Vector(1.5, 2.6, 1.1));
+    }
+
+    @Test
+    void greaterThanEqTest()    {
+        assertTrue(MathUtils.greaterThanEq(new Vector(3.1, 5.1, 2.0), new Vector(3.0, 5.0, 2.0)));
+        assertFalse(MathUtils.greaterThanEq(new Vector(3.0, 5.0, 2.0), new Vector(3.1, 5.1, 2.0)));
+    }
+
+    @Test
+    void lessThanEqTest()    {
+        assertTrue(MathUtils.lessThanEq(new Vector(3.0, 5.0, 2.0), new Vector(3.1, 5.1, 2.0)));
+        assertFalse(MathUtils.lessThanEq(new Vector(3.1, 5.1, 2.0), new Vector(3.0, 5.0, 2.0)));
+    }
+}
