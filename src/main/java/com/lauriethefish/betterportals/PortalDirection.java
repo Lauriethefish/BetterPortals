c82d85c709016cb68000f856ab217cb51ff40e41
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-09-12 07:31:11 +0100

    Added UP_DOWN variant, added toVector method to get the normal of this direction, moved VisibilityChecker.orientVector here as an instance method

--- src/main/java/com/lauriethefish/betterportals/PortalDirection.java
+++ src/main/java/com/lauriethefish/betterportals/PortalDirection.java
@@ -1,7 +1,34 @@
 package com.lauriethefish.betterportals;
 
+import org.bukkit.util.Vector;
+
 // Two directions of a portal, used for more ergonomic handling
 public enum PortalDirection {
-    EAST_WEST, // Along the x axis
-    NORTH_SOUTH // Along the z axis
+    NORTH_SOUTH(new Vector(1.0, 0.0, 0.0)), // Along the z axis
+    UP_DOWN(new Vector(0.0, 1.0, 0.0)), // Along the y axis
+    EAST_WEST(new Vector(0.0, 0.0, 1.0)); // Along the x axis
+
+    // The direction vector is a normal facing out of the portal
+    private final Vector direction;
+    private PortalDirection(Vector direction)   {
+        this.direction = direction;
+    }
+
+    public Vector toVector()    {
+        return direction;
+    }
+
+    // Swaps the coordinates of the given vector if this portal is on the NORTH_SOUTH or UP_DOWN axis
+    // This is used when changing blocks around portals, since we cannot just use the coordinates regularly
+    public Vector swapVector(Vector vec)    {
+        switch(this)    {
+            case NORTH_SOUTH:
+                return new Vector(vec.getZ(), vec.getY(), vec.getX());
+            case UP_DOWN:
+                return new Vector(vec.getX(), vec.getZ(), vec.getY());
+            case EAST_WEST:
+                return vec.clone();
+        }
+        return null;
+    }
 }
