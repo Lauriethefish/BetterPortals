2ae3b43cf954dcdf2d258298d065cee9197935cb
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-09-15 15:36:48 +0100

    Added inverseRotationAxis parameter This is necessary as otherwise we rotate around the wrong axis when moving between NORTH and SOUTH portals for example

--- src/main/java/com/lauriethefish/betterportals/portal/PortalDirection.java
+++ src/main/java/com/lauriethefish/betterportals/portal/PortalDirection.java
@@ -5,36 +5,48 @@ import org.bukkit.util.Vector;
 
 // Two directions of a portal, used for more ergonomic handling
 public enum PortalDirection {
-    NORTH_SOUTH(new Vector(1.0, 0.0, 0.0)), // Along the z axis
-    UP_DOWN(new Vector(0.0, 1.0, 0.0)), // Along the y axis
-    EAST_WEST(new Vector(0.0, 0.0, 1.0)); // Along the x axis
+    UP(new Vector(0.0, 1.0, 0.0), new Vector(1.0, 0.0, 0.0)),
+    DOWN(new Vector(0.0, -1.0, 0.0), new Vector(1.0, 0.0, 0.0)),
+    NORTH(new Vector(0.0, 0.0, 1.0), new Vector(0.0, 1.0, 0.0)),
+    SOUTH(new Vector(0.0, 0.0, -1.0), new Vector(0.0, 1.0, 0.0)),
+    EAST(new Vector(1.0, 0.0, 0.0), new Vector(0.0, 1.0, 0.0)),
+    WEST(new Vector(-1.0, 0.0, 0.0), new Vector(0.0, 1.0, 0.0));
 
     // The direction vector is a normal facing out of the portal
     private final Vector direction;
-    private PortalDirection(Vector direction)   {
+    // This vector is used if we need to rotate the portal view round 180 degrees because the origin and destination are in exact opposite direction
+    private final Vector inversionRotationAxis;
+    private PortalDirection(Vector direction, Vector inversionRotationAxis)   {
         this.direction = direction;
+        this.inversionRotationAxis = inversionRotationAxis;
     }
 
     public Vector toVector()    {
         return direction;
     }
 
+    public Vector getInversionRotationAxis()    {
+        return inversionRotationAxis;
+    }
+
     // Swaps the coordinates of the given vector if this portal is on the NORTH_SOUTH or UP_DOWN axis
     // This is used when changing blocks around portals, since we cannot just use the coordinates regularly
     public Vector swapVector(Vector vec)    {
         switch(this)    {
-            case NORTH_SOUTH:
+            case EAST:
                 return new Vector(vec.getZ(), vec.getY(), vec.getX());
-            case UP_DOWN:
+            case UP:
                 return new Vector(vec.getX(), vec.getZ(), vec.getY());
-            case EAST_WEST:
+            case NORTH:
+                return vec.clone();
+            case WEST:
+                return new Vector(vec.getZ(), vec.getY(), vec.getX());
+            case DOWN:
+                return new Vector(vec.getX(), vec.getZ(), vec.getY());
+            case SOUTH:
                 return vec.clone();
         }
         return null;
-    }
-
-    public void invert()    {
-        direction.multiply(-1.0);
     }
 
     public Location swapLocation(Location loc)  {
