a40de1d699aab28cabbfdc4041b12aa65527dfb6
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-09-20 06:55:11 +0100

    Added field for storing the world. Added method to get the Bukkit chunk. Added method to get a set of all ChunkCoordIntPairs in the area between A and B. Added toString method for debugging

--- src/main/java/com/lauriethefish/betterportals/multiblockchange/ChunkCoordIntPair.java
+++ src/main/java/com/lauriethefish/betterportals/multiblockchange/ChunkCoordIntPair.java
@@ -1,18 +1,63 @@
 package com.lauriethefish.betterportals.multiblockchange;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.lauriethefish.betterportals.ReflectUtils;
 
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.util.Vector;
 
 // A wrapper over the coordinates of a chunk that automatically converts them to the NMS version
+// This can also just be used to hold the coordinates of a chunk
 public class ChunkCoordIntPair {
+    public World world; // This can be null
     public int x;
     public int z;
+
+    public ChunkCoordIntPair(World world, int chunkX, int chunkZ) {
+        this.world = world;
+        x = chunkX;
+        z = chunkZ;
+    }
+
+    public ChunkCoordIntPair(Location location) {
+        x = location.getBlockX() >> 4;
+        z = location.getBlockZ() >> 4;
+        world = location.getWorld();
+    }
+
     public ChunkCoordIntPair(Vector location) {
         x = location.getBlockX() >> 4;
         z = location.getBlockZ() >> 4;
+    }
+
+    public ChunkCoordIntPair(Chunk chunk)   {
+        x = chunk.getX();
+        z = chunk.getZ();
+        world = chunk.getWorld();
+    }
+
+    public Chunk getChunk() {
+        return world.getChunkAt(x, z);
+    }
+
+    public static Set<ChunkCoordIntPair> findArea(Location a, Location b)   {
+        // Find the coordinates of the two locations
+        ChunkCoordIntPair low = new ChunkCoordIntPair(a);
+        ChunkCoordIntPair high = new ChunkCoordIntPair(b);
+
+        // Loop through all the chunks between them and add them to the set
+        Set<ChunkCoordIntPair> result = new HashSet<>();
+        for(int z = low.z; z <= high.z; z++)    {
+            for(int x = low.x; x <= high.x; x++)    {
+                result.add(new ChunkCoordIntPair(a.getWorld(), x, z));   
+            }
+        }
+        return result;
     }
 
     // Makes an NMS ChunkCoordIntPair from this object
@@ -30,6 +75,11 @@ public class ChunkCoordIntPair {
         }
         ChunkCoordIntPair chunkCoordIntPair = (ChunkCoordIntPair) o;
         return x == chunkCoordIntPair.x && z == chunkCoordIntPair.z;
+    }
+
+    @Override
+    public String toString()    {
+        return String.format("x: %d, z: %d", x, z);
     }
 
     @Override
