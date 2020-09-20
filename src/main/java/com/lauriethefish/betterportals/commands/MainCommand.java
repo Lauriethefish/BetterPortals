5a870f4dc492234af4370cb6bae8230928dd814e
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-09-20 07:00:28 +0100

    Changed to properly disable and reenable the plugin in the reload subcommand. Changed chatPrefix to PREFIX

--- src/main/java/com/lauriethefish/betterportals/commands/MainCommand.java
+++ src/main/java/com/lauriethefish/betterportals/commands/MainCommand.java
@@ -1,18 +1,16 @@
 package com.lauriethefish.betterportals.commands;
 
-import java.util.HashMap;
-
 import com.lauriethefish.betterportals.BetterPortals;
-import com.lauriethefish.betterportals.PlayerData;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginManager;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class MainCommand implements CommandExecutor {
-    private static final String chatPrefix = ChatColor.GRAY + "[" + ChatColor.GREEN + "BetterPortals" + ChatColor.GRAY + "]" + ChatColor.GREEN + " ";
+    private static final String PREFIX = ChatColor.GRAY + "[" + ChatColor.GREEN + "BetterPortals" + ChatColor.GRAY + "]" + ChatColor.GREEN + " ";
 
     private BetterPortals pl;
     public MainCommand(BetterPortals pl)   {
@@ -34,21 +32,11 @@ public class MainCommand implements CommandExecutor {
         }
 
         if(subcommand.equals("reload")) {
-            // Reset each player's block states, as the portal size may have changed
-            for(PlayerData player : pl.players.values())    {
-                if(player.player.isOnline())    {
-                    // We don't use resetSurroundingBlockStates here, since that would send packets that change the blocks back, causing the portal to flicker
-                    player.surroundingPortalBlockStates = new HashMap<>();
-                }
-            }
+            PluginManager pm = pl.getServer().getPluginManager();
+            pm.disablePlugin(pl);
+            pm.enablePlugin(pl);
 
-            // Reload the config, printing an error if it fails
-            pl.reloadConfig(); // First reload the file on disc
-            if(!pl.loadConfig())    { // Then load all the values
-                sender.sendMessage(chatPrefix + ChatColor.RED + "Error reloading the config file. Please check config.yml for errors.");
-            }   else    {
-                sender.sendMessage(chatPrefix + "Config reloaded.");
-            }
+            sender.sendMessage(PREFIX + " Reloaded plugin");
         }
 
         return true;
