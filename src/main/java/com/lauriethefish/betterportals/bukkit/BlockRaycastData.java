187ac3250118abda91cf699187d3c8691db09fe3
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-11-21 09:44:57 +0000

    Changed to use the combined block ID until IBlockData is needed. This allows cross-server portals, since now BlockRaycastData can be serializable.

--- src/main/java/com/lauriethefish/betterportals/bukkit/BlockRaycastData.java
+++ src/main/java/com/lauriethefish/betterportals/bukkit/BlockRaycastData.java
@@ -13,30 +13,84 @@ import lombok.Getter;
 
 // Stores data about a block surrounding the portal that is in the HashMap of viewable blocks
 public class BlockRaycastData {
-    private static Object edgeData = makeEdgeData();
-    private static Object makeEdgeData()    {
-        // Different colours of concrete depend on version
-        if(ReflectUtils.isLegacy)  {
-            return getNMSData(Material.valueOf("CONCRETE"), (byte) 15);
+    private static int edgeDataCombinedId = makeEdgeData();
+
+    private static int makeEdgeData() {
+        // Different colours of concrete depend on version if(ReflectUtils.isLegacy)
+        if(ReflectUtils.isLegacy)   {
+            return getCombinedId(Material.valueOf("CONCRETE"), (byte) 15);
         }   else    {
-            return getNMSData(Material.BLACK_CONCRETE);
+            return getCombinedId(Material.BLACK_CONCRETE, (byte) 0);
         }
     }
 
     @Getter private Vector originVec;
-    @Getter private Object originData;
-    @Getter private Object destData;
-    public BlockRaycastData(BlockRotator rotator, Location originLoc, Location destLoc, boolean edge)   {
-        // Find the location at the exact center of the origin block, used for portal intersection checking
+    private transient Object originDataCache;
+    private transient Object destDataCache;
+    private int originDataCombinedId;
+    private int destDataCombinedId;
+
+    public BlockRaycastData(BlockRotator rotator, Location originLoc, Location destLoc, boolean edge) {
+        // Find the location at the exact center of the origin block, used for portal
+        // intersection checking
         this.originVec = MathUtils.moveToCenterOfBlock(originLoc.toVector());
-        this.originData = getNMSData(originLoc.getBlock().getState()); // Find the IBlockData in the origin block
+        this.originDataCombinedId = getCombinedId(originLoc.getBlock().getState()); // Find the IBlockData in the origin block
 
         // Rotate the block at the other side if we need to, so it is at the origin
         BlockState destBlockState = destLoc.getBlock().getState();
         rotator.rotateToOrigin(destBlockState);
-        this.destData = edge ? edgeData : getNMSData(destBlockState);
+        this.destDataCombinedId = edge ? edgeDataCombinedId : getCombinedId(destBlockState);
     }
 
+    // Fetches the NMS IBlockData if we need to, otherwise just returns the cached data
+    public Object getOriginData() {
+        if(originDataCache == null) {originDataCache = getNMSData(originDataCombinedId);}
+        return originDataCache;
+    }
+
+    public Object getDestData() {
+        if(destDataCache == null) {destDataCache = getNMSData(destDataCombinedId);}
+        return destDataCache;
+    }
+
+    // Finds the combined block ID and data from a BlockState
+    @SuppressWarnings("deprecation")
+    public static int getCombinedId(BlockState state) {
+        return getCombinedId(state.getType(), state.getRawData());
+    }
+
+    // Finds the combined ID for a material and raw data byte
+    @SuppressWarnings("deprecation")
+    private static int getCombinedId(Material material, byte rawData) {
+        return material.getId() + (rawData << 12);
+    }
+
+    private static Method getNMSDataFromIdMethod = ReflectUtils.findMethod(ReflectUtils.getMcClass("Block"), "getByCombinedId",
+            new Class[] { int.class });
+    
+    // Finds the NMS IBlockData object from a combined block ID and data
+    private static Object getNMSData(int combinedId) {
+        try {
+            return getNMSDataFromIdMethod.invoke(null, combinedId);
+        } catch (ReflectiveOperationException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Object getNMSData(Material material, byte rawData) {
+        return getNMSData(getCombinedId(material, rawData));
+    }
+
+    public static Object getNMSData(Material material) {
+        return getNMSData(material, (byte) 0);
+    }
+
+    public static Object getNMSData(BlockState state) {
+        return getNMSData(getCombinedId(state));
+    }
+
+    /*
     public static Object getNMSData(Material mat)  {
         Object block = ReflectUtils.runMethod(null, ReflectUtils.getBukkitClass("util.CraftMagicNumbers"), "getBlock", new Class[]{Material.class}, new Object[]{mat});
         return ReflectUtils.getField(block, "blockData");
@@ -73,4 +127,5 @@ public class BlockRaycastData {
             return null;
         }
     }
+    */
 }
