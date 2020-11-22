8ae9b82925447e114f02b3796cf7d372a02c4ce5
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-11-22 20:58:32 +0000

    Request for teleporting players when going through a cross-server portal.

---
+++ src/main/java/com/lauriethefish/betterportals/network/TeleportPlayerRequest.java
@@ -0,0 +1,29 @@
+package com.lauriethefish.betterportals.network;
+
+import java.util.UUID;
+
+import lombok.Getter;
+
+// Sent by a server to get the proxy to teleport the player to the destination of a portal
+// This is also sent to destServer in order to correctly position the player once they join
+public class TeleportPlayerRequest implements Request   {
+    private static final long serialVersionUID = 1113165482649027108L;
+    
+    @Getter private UUID playerId;
+
+    @Getter private String destServer;
+    @Getter private String destWorldName;
+
+    @Getter private double destX;
+    @Getter private double destY;
+    @Getter private double destZ;
+
+    public TeleportPlayerRequest(UUID playerId, String destServer, String destWorldName, double destX, double destY, double destZ) {
+        this.playerId = playerId;
+        this.destServer = destServer;
+        this.destWorldName = destWorldName;
+        this.destX = destX;
+        this.destY = destY;
+        this.destZ = destZ;
+    }
+}
