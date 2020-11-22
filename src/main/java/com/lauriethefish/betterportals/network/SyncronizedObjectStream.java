8c98209ffa047dcd3012661da5b52e5716d39f0b
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-11-22 14:12:47 +0000

    Fixed synchronization issues.

--- src/main/java/com/lauriethefish/betterportals/network/SyncronizedObjectStream.java
+++ src/main/java/com/lauriethefish/betterportals/network/SyncronizedObjectStream.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
@@ -11,25 +12,36 @@ import java.util.concurrent.locks.ReentrantLock;
 public class SyncronizedObjectStream {
     private ObjectInputStream inputStream;
     private ObjectOutputStream outputStream;
-    private ReentrantLock outputLock = new ReentrantLock(true); // Use a fair lock to guarantee ordering of written objects
 
-    private List<Object> skippedList = new ArrayList<>();
+    private ReentrantLock outputLock = new ReentrantLock(true); // Use a fair lock to guarantee ordering of written objects
+    private ReentrantLock inputLock = new ReentrantLock(true);
+
+    private List<Object> skippedList = Collections.synchronizedList(new ArrayList<>());
 
     public SyncronizedObjectStream(ObjectInputStream inputStream, ObjectOutputStream outputStream) {
         this.inputStream = inputStream;
         this.outputStream = outputStream;
     }
 
-    // Reads the next Object of type type from the stream, or fetches it from skippedList if it was received previously
-    public synchronized Object readNextOfType(Class<?> type) throws IOException, ClassNotFoundException    {
-        // Loop through any objects that were skipped because they weren't of the right type
-        Iterator<Object> iterator = skippedList.iterator();
-        while(iterator.hasNext()) {
-            // Find the first of the correct type, then remove and return it
-            Object obj = iterator.next();
-            if(type.isInstance(obj)) {
-                iterator.remove();
-                return obj;
+    // Reads the next Object of type type from the stream, or fetches it from
+    // skippedList if it was received previously
+    public Object readNextOfType(Class<?> type) throws IOException, ClassNotFoundException {
+        while (!inputLock.tryLock() || skippedList.size() > 0) {
+            // Loop through any objects that were skipped because they weren't of the right type
+            Iterator<Object> iterator = skippedList.iterator();
+            while (iterator.hasNext()) {
+                // Find the first of the correct type, then remove and return it
+                Object obj = iterator.next();
+                if (type.isInstance(obj)) {
+                    iterator.remove();
+                    return obj;
+                }
+            }
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
             }
         }
 
@@ -37,6 +49,7 @@ public class SyncronizedObjectStream {
         while(true) {
             Object obj = inputStream.readObject();
             if(type.isInstance(obj)) { // If the object read had the right type, return it
+                inputLock.unlock();
                 return obj;
             }   else    {
                 skippedList.add(obj); // Otherwise add it to the skipped list
