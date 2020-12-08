7d7324eeacf2a96c3f0f95a841784643d9b7f131
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-12-08 18:15:41 +0000

    Handles requesting data from the destination server on another thread.

---
+++ src/main/java/com/lauriethefish/betterportals/bukkit/portal/blockarray/ExternalUpdateWorker.java
@@ -0,0 +1,59 @@
+package com.lauriethefish.betterportals.bukkit.portal.blockarray;
+
+import com.lauriethefish.betterportals.bukkit.BetterPortals;
+import com.lauriethefish.betterportals.bukkit.network.BlockDataUpdateResult;
+import com.lauriethefish.betterportals.bukkit.network.GetBlockDataArrayRequest;
+import com.lauriethefish.betterportals.network.Response.RequestException;
+
+import lombok.Getter;
+
+public class ExternalUpdateWorker implements Runnable {
+    private BetterPortals pl;
+    @Getter private CachedViewableBlocksArray cachedArray;
+    private GetBlockDataArrayRequest request;
+
+    private volatile BlockDataUpdateResult result = null;
+    private volatile boolean failed = false;
+
+    public ExternalUpdateWorker(BetterPortals pl, GetBlockDataArrayRequest request, CachedViewableBlocksArray cachedArray) {
+        this.pl = pl;
+        this.cachedArray = cachedArray;
+        this.request = request;
+
+        new Thread(this).start();
+    }
+
+    public boolean hasFinished() {
+        return result != null;
+    }
+
+    public boolean hasFailed() {
+        return failed;
+    }
+
+    // Called to process the BlockDataUpdateResult on the main thread once it has been fetched
+    public void finishUpdate() {
+        cachedArray.checkForChanges(request, true, false);
+        cachedArray.processExternalUpdate(request, result);
+    }
+
+    @Override
+    public void run() {
+        String destinationServer = request.getDestPos().getServerName();
+        // Make sure we're actually connected
+        if(pl.getNetworkClient() == null) {
+            pl.getLogger().warning("Update for external portal failed - bungeecord is not enabled!");
+            return;
+        }
+
+        try {
+            // Send the request to the destination server
+            Object rawResult = pl.getNetworkClient().sendRequestToServer(request, destinationServer);
+            result = (BlockDataUpdateResult) rawResult;
+        } catch (RequestException ex) {
+            pl.getLogger().warning("An error occured while fetching the blocks for an external portal. This portal will not activate.");
+            failed = true;
+            ex.printStackTrace();
+        }
+    }
+}
