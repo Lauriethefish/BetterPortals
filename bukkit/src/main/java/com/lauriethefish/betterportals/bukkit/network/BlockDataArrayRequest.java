79e8fa36721fad2e8ce4c5ced53415ac8bf56802
Laurie Fay <lauriethefish@outlook.com>
2021-01-08 06:58:53 +0000

    Added mode for choosing whether to update or remove the block array.

--- bukkit/src/main/java/com/lauriethefish/betterportals/bukkit/network/BlockDataArrayRequest.java
+++ bukkit/src/main/java/com/lauriethefish/betterportals/bukkit/network/BlockDataArrayRequest.java
@@ -8,17 +8,29 @@ import com.lauriethefish.betterportals.network.Request;
 
 import lombok.Getter;
 
-// Used to fetch the array of block data from another server for cross-server portals
+// Used to fetch the array (or delete it) of block data from another server for cross-server portals
 public class BlockDataArrayRequest implements Request    {
     private static final long serialVersionUID = 933279153991293225L;
+    // Chooses whether to fetch the array, or delete it incase of deactivation
+    public enum Mode {
+        GET_OR_UPDATE,
+        CLEAR
+    }
     
-    @Getter private PortalPosition originPos;
-    @Getter private PortalPosition destPos;
+    @Getter private final PortalPosition originPos;
+    @Getter private final PortalPosition destPos;
+    @Getter private final Mode mode;
     private transient PortalTransformations transformations = null;
 
-    public BlockDataArrayRequest(PortalPosition originPos, PortalPosition destPos) {
+    public BlockDataArrayRequest(PortalPosition originPos, PortalPosition destPos, Mode mode) {
         this.originPos = originPos;
         this.destPos = destPos;
+        this.mode = mode;
+    }
+
+    // Constructs a get request more easily
+    public BlockDataArrayRequest(PortalPosition originPos, PortalPosition destPos) {
+        this(originPos, destPos, Mode.GET_OR_UPDATE);
     }
 
     // Creates the PortalTransformations if they haven't been made already, then returns them
