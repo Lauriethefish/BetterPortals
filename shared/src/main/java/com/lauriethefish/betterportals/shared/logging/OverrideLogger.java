d3bf0d9725f36afda3a6308af3d64394bc81217d
Lauriethefish <lauriethefish@outlook.com>
2021-05-13 17:39:59 +0100

    Make all log prefixes 3 characters Makes logs line up better, since FINE, FINER and FINEST were different lengths before. Now they're FNE, FNR FST

--- shared/src/main/java/com/lauriethefish/betterportals/shared/logging/OverrideLogger.java
+++ shared/src/main/java/com/lauriethefish/betterportals/shared/logging/OverrideLogger.java
@@ -1,13 +1,30 @@
 package com.lauriethefish.betterportals.shared.logging;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 /**
  * Takes a parent logger and prints any log messages at DEBUG level or below by just using INFO
- * This is necessary because there's no good way to set bukkit's plugin logger to print debug messages
+ * This is necessary because there's no good way to set bukkit's plugin logger to print debug messages (you have to go through the whole logger tree using reflection and I could never get it to work)
  */
 public class OverrideLogger extends Logger {
+    /**
+     * Used only if the log is printed at INFO level using a prefix.
+     * Makes the logs line up more by always using 3 characters for the prefix
+     */
+    private static final Map<Level, String> logLevelNames = new HashMap<>();
+    static {
+        logLevelNames.put(Level.SEVERE, "SEV");
+        logLevelNames.put(Level.WARNING, "WRN");
+        logLevelNames.put(Level.INFO, "INF");
+        logLevelNames.put(Level.CONFIG, "CFG");
+        logLevelNames.put(Level.FINE, "FNE");
+        logLevelNames.put(Level.FINER, "FNR");
+        logLevelNames.put(Level.FINEST, "FST");
+    }
+
     private final java.util.logging.Logger logger;
 
     public OverrideLogger(java.util.logging.Logger logger) {
@@ -25,7 +42,11 @@ public class OverrideLogger extends Logger {
         Level originalLevel = record.getLevel();
         if(originalLevel.intValue() < Level.INFO.intValue()) {
             record.setLevel(Level.INFO);
-            record.setMessage(String.format("[%s] %s", originalLevel.getName(), record.getMessage()));
+            String levelName = logLevelNames.get(originalLevel);
+            if(levelName == null) {
+                levelName = originalLevel.getName();
+            }
+            record.setMessage(String.format("[%s] %s", levelName, record.getMessage()));
         }
 
         logger.log(record);
